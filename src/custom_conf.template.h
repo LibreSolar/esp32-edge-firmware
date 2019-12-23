baa61109e6121831770c4637b42dcf2c572f9e56
Martin Jaeger <17674105+martinjaeger@users.noreply.github.com>
2019-12-23 10:32:00 +0100

    WiFi and HTTP post to emoncms added

---
+++ src/custom_conf.template.h
@@ -0,0 +1,10 @@
+
+#define CONFIG_WIFI_SSID        "your_ssid"
+#define CONFIG_WIFI_PASSWORD    "pass"
+
+#define EMONCMS_HOST        "emoncms.org"
+#define EMONCMS_PORT        "80"
+#define EMONCMS_URL         "/emoncms/input/post"
+#define EMONCMS_APIKEY      "your_api_key"
+#define EMONCMS_NODE_MPPT   "mppt"
+#define EMONCMS_NODE_BMS    "bms"
--- src/main.c
+++ src/main.c
@@ -2,14 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/semphr.h"
+#include "freertos/event_groups.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "driver/can.h"
+#include "nvs_flash.h"
 #include "sdkconfig.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+
+#include "custom_conf.h"
 
 #define GPIO_LED        18
 #define GPIO_CAN_RX     4
@@ -17,6 +31,17 @@
 #define GPIO_CAN_STB    12
 
 #define RX_TASK_PRIO    9       // receiving task priority
+static const char* TAG = "main";    // for logging
+
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+static EventGroupHandle_t wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about one
+   event - are we connected to the AP with an IP? */
+const int CONNECTED_BIT = BIT0;
+
+static bool update_bms_received = false;
+static bool update_mppt_received = false;
 
 static const can_timing_config_t t_config = CAN_TIMING_CONFIG_250KBITS();
 static const can_filter_config_t f_config = CAN_FILTER_CONFIG_ACCEPT_ALL();
@@ -98,6 +123,7 @@ static void can_receive_task(void *arg)
                         data_obj_bms[i].len = message.data_length_code;
                     }
                 }
+                update_bms_received = true;
             }
             else if (node_id == 10) {
                 for (int i = 0; i < sizeof(data_obj_mppt)/sizeof(DataObject); i++) {
@@ -106,9 +132,11 @@ static void can_receive_task(void *arg)
                         data_obj_mppt[i].len = message.data_length_code;
                     }
                 }
+                update_mppt_received = true;
             }
+
             /*
-            printf("Message from node %u received with priority %u. Data object 0x%.2x = 0x",
+            printf("CAN Message from node %u received with priority %u. Data object 0x%.2x = 0x",
                 node_id, msg_priority, data_object_id);
             if (!(message.flags & CAN_MSG_FLAG_RTR)) {
                 for (int i = 0; i < message.data_length_code; i++) {
@@ -204,30 +232,168 @@ static int generate_json_string(char *buf, size_t len, DataObject *objs, size_t 
     return pos;
 }
 
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch (event->event_id) {
+        case SYSTEM_EVENT_STA_START:
+            esp_wifi_connect();
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            /* This is a workaround as ESP32 WiFi libs don't currently
+            auto-reassociate. */
+            esp_wifi_connect();
+            xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
+}
+
+static void initialise_wifi(void)
+{
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_WIFI_SSID,
+            .password = CONFIG_WIFI_PASSWORD,
+        },
+    };
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_ps(WIFI_PS_NONE) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+}
+
+static int send_emoncms(struct addrinfo *res, const char *node_name, DataObject *objs, size_t num_objs)
+{
+    static char buf[500];
+    static char http_body[500];
+    char recv_buf[64];
+
+    const char *http_header =
+        "POST " EMONCMS_URL " HTTP/1.1\r\n"
+        "Host: " EMONCMS_HOST "\r\n"
+        "Authorization: " EMONCMS_APIKEY "\r\n"
+        "User-Agent: esp-idf/1.0 esp32\r\n"
+        "Content-Type: application/x-www-form-urlencoded\r\n"
+        "Connection: close\r\n";
+
+    int pos = snprintf(http_body, sizeof(http_body), "node=%s&json=", node_name);
+    pos += generate_json_string(&http_body[pos], sizeof(http_body) - pos, objs, num_objs);
+    printf("HTTP body for %s: %s\n", node_name, http_body);
+
+    int s = socket(res->ai_family, res->ai_socktype, 0);
+    if (s < 0) {
+        ESP_LOGE(TAG, "... Failed to allocate socket.");
+        freeaddrinfo(res);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        return -1;
+    }
+    ESP_LOGI(TAG, "... allocated socket\r\n");
+
+    if (connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+        ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+        close(s);
+        freeaddrinfo(res);
+        vTaskDelay(4000 / portTICK_PERIOD_MS);
+        return -1;
+    }
+    ESP_LOGI(TAG, "... connected");
+    freeaddrinfo(res);
+
+    int err = write(s, http_header, strlen(http_header));
+    sprintf(buf, "Content-Length: %d\r\n\r\n", strlen(http_body));
+    err += write(s, buf, strlen(buf));
+    err += write(s, http_body, strlen(http_body));
+    err += write(s, "\r\n", 1);
+
+    if (err < 0) {
+        ESP_LOGE(TAG, "... socket send failed");
+        close(s);
+        vTaskDelay(4000 / portTICK_PERIOD_MS);
+        return -1;
+    }
+    ESP_LOGI(TAG, "... socket send success");
+
+    // Read HTTP response
+    int resp;
+    do {
+        bzero(recv_buf, sizeof(recv_buf));
+        resp = read(s, recv_buf, sizeof(recv_buf)-1);
+        for(int i = 0; i < resp; i++) {
+            putchar(recv_buf[i]);
+        }
+    } while (resp > 0);
+
+    ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d\r\n", resp, errno);
+    close(s);
+    return 1;
+}
+
 static void http_get_task(void *arg)
 {
-    char buf[500];
-    int len;
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    struct addrinfo *res;
+    struct in_addr *addr;
 
-    while(1) {
+    while (1) {
+
+        // wait until we receive an update
+        while (update_bms_received == false && update_mppt_received == false) {
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+
+        // Wait for the callback to set the CONNECTED_BIT in the event group.
+        xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+        ESP_LOGI(TAG, "Connected to AP");
+
+        int err = getaddrinfo(EMONCMS_HOST, EMONCMS_PORT, &hints, &res);
+
+        if (err != 0 || res == NULL) {
+            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        // Code to print the resolved IP.
+        // Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code
+        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+        if (update_bms_received) {
+            update_bms_received = false;
+            send_emoncms(res, EMONCMS_NODE_BMS,
+                data_obj_bms, sizeof(data_obj_bms)/sizeof(DataObject));
+        }
+
+        if (update_mppt_received) {
+            update_mppt_received = false;
+            send_emoncms(res, EMONCMS_NODE_MPPT,
+                data_obj_mppt, sizeof(data_obj_mppt)/sizeof(DataObject));
+        }
+
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-
-        // BMS data objects
-        len = generate_json_string(buf, sizeof(buf), data_obj_bms, sizeof(data_obj_bms)/sizeof(DataObject));
-        if (len > 0) {
-            printf("BMS data: %s\n", buf);
-        }
-
-        // MPPT data objects
-        len = generate_json_string(buf, sizeof(buf), data_obj_mppt, sizeof(data_obj_mppt)/sizeof(DataObject));
-        if (len > 0) {
-            printf("MPPT data: %s\n", buf);
-        }
     }
 }
 
+
 void app_main(void)
 {
+    nvs_flash_init();
+
     // configure the pad to GPIO and set direction
     gpio_pad_select_gpio(GPIO_LED);
     gpio_set_direction(GPIO_LED, GPIO_MODE_OUTPUT);
@@ -259,5 +425,6 @@ void app_main(void)
 
     xTaskCreatePinnedToCore(can_receive_task, "CAN_rx", 4096, NULL, RX_TASK_PRIO, NULL, tskNO_AFFINITY);
 
+    initialise_wifi();
     xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
 }
