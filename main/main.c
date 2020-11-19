b5cb7261265dd6fe532be3953c76217c53c43610
Leonard Möller <leonard.moeller@live.com>
2020-11-19 16:41:17 +0100

    added provisioning via bluetooth, increased app partition to 2M

--- main/main.c
+++ main/main.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -75,3 +76,83 @@ void app_main(void)
         NULL, 5, NULL);
 #endif
 }
+=======
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2019 Martin Jäger / Libre Solar
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "driver/gpio.h"
+#include "nvs_flash.h"
+#include "sdkconfig.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+
+#include "ts_serial.h"
+#include "can.h"
+#include "emoncms.h"
+#include "wifi.h"
+#include "web_fs.h"
+#include "web_server.h"
+#include "provisioning.h"
+
+#define RX_TASK_PRIO    9       // receiving task priority
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    provision();
+    // configure the LED pad as GPIO and set direction
+    gpio_pad_select_gpio(CONFIG_GPIO_LED);
+    gpio_set_direction(CONFIG_GPIO_LED, GPIO_MODE_OUTPUT);
+    gpio_set_level(CONFIG_GPIO_LED, 1);
+
+    // wait 3s to open serial terminal after flashing finished
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+ 	printf("Booting Libre Solar Data Manager...\n");
+
+    init_fs();
+
+#if CONFIG_THINGSET_CAN
+    can_setup();
+    xTaskCreatePinnedToCore(can_receive_task, "CAN_rx", 4096,
+        NULL, RX_TASK_PRIO, NULL, tskNO_AFFINITY);
+#endif
+
+#if CONFIG_THINGSET_SERIAL
+    ts_serial_setup();
+    xTaskCreatePinnedToCore(ts_serial_rx_task, "ts_serial_rx", 4096,
+        NULL, RX_TASK_PRIO, NULL, tskNO_AFFINITY);
+#endif
+
+    //wifi_connect();
+
+    start_web_server("/www");
+
+#if CONFIG_EMONCMS
+    xTaskCreate(&emoncms_post_task, "emoncms_post_task", 4096,
+        NULL, 5, NULL);
+#endif
+}
+>>>>>>> added provisioning via bluetooth, increased app partition to 2M
---
+++ main/provisioning.c
@@ -0,0 +1,142 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2020 Martin Jäger / Libre Solar
+ */
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+
+#include <wifi_provisioning/manager.h>
+
+#include <wifi_provisioning/scheme_ble.h>
+#include "provisioning.h"
+
+static const char *TAG = "prov";
+
+const int WIFI_CONNECTED_EVENT = BIT0;
+static EventGroupHandle_t wifi_event_group;
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                          int event_id, void* event_data)
+{
+    if(event_base == WIFI_PROV_EVENT) {
+        switch(event_id) {
+            case WIFI_PROV_START: {
+                ESP_LOGI(TAG, "Started provisioning routine");
+                break;
+            }
+            case WIFI_PROV_CRED_RECV: {
+                wifi_sta_config_t *wifi_sta_cfg = (wifi_sta_config_t *)event_data;
+                ESP_LOGI(TAG, "Received Wi-Fi credentials"
+                         "\n\tSSID     : %s\n\tPassword : %s",
+                         (const char *) wifi_sta_cfg->ssid,
+                         (const char *) wifi_sta_cfg->password);
+                break;
+            }
+            case WIFI_PROV_CRED_FAIL: {
+                wifi_prov_sta_fail_reason_t *reason = (wifi_prov_sta_fail_reason_t *)event_data;
+                ESP_LOGE(TAG, "Provisioning failed!\n\tReason : %s"
+                         "\n\tPlease reset to factory and retry provisioning",
+                         (*reason == WIFI_PROV_STA_AUTH_ERROR) ?
+                         "Wi-Fi station authentication failed" : "Wi-Fi access-point not found");
+                break;
+            }
+            case WIFI_PROV_CRED_SUCCESS:
+                ESP_LOGI(TAG, "Provisioning successful");
+                break;
+            case WIFI_PROV_END:
+                /* De-initialize manager once provisioning is finished */
+                wifi_prov_mgr_deinit();
+                break;
+            default:
+                break;
+        }
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "Connected with IP Address:" IPSTR, IP2STR(&event->ip_info.ip));
+        /* Signal main application to continue execution */
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_EVENT);
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGI(TAG, "Disconnected. Connecting to the AP again...");
+        esp_wifi_connect();
+    }
+}
+
+static void get_device_service_name(char *service_name, size_t max)
+{
+    uint8_t eth_mac[6];
+    const char *ssid_prefix = "PROV_";
+    esp_wifi_get_mac(WIFI_IF_STA, eth_mac);
+    snprintf(service_name, max, "%s%02X%02X%02X",
+             ssid_prefix, eth_mac[3], eth_mac[4], eth_mac[5]);
+}
+
+void provision(void)
+{
+    wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_PROV_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    /* Configuration for the provisioning manager, we don't need BT or BLE after provisioning */
+    wifi_prov_mgr_config_t config = {
+        .scheme = wifi_prov_scheme_ble,
+        .scheme_event_handler = WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM
+        };
+    ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
+
+    bool provisioned = false;
+    /* Let's find out if the device is provisioned */
+    ESP_ERROR_CHECK(wifi_prov_mgr_is_provisioned(&provisioned));
+
+    if (!provisioned) {
+        ESP_LOGI(TAG, "Starting provisioning");
+
+        /* What is the Device Service Name that we want
+         * This translates to device name
+         */
+        char service_name[12];
+        get_device_service_name(service_name, sizeof(service_name));
+        wifi_prov_security_t security = WIFI_PROV_SECURITY_1;
+        const char *pop = "LibreSolar";
+        const char *service_key = NULL;
+
+        uint8_t custom_service_uuid[] = {
+            /* LSB <---------------------------------------
+             * ---------------------------------------> MSB */
+            0xb4, 0xdf, 0x5a, 0x1c, 0x3f, 0x6b, 0xf4, 0xbf,
+            0xea, 0x4a, 0x82, 0x03, 0x04, 0x90, 0x1a, 0x02,
+        };
+        wifi_prov_scheme_ble_set_service_uuid(custom_service_uuid);
+
+        ESP_ERROR_CHECK(wifi_prov_mgr_start_provisioning(security, pop, service_name, service_key));
+    } else {
+        ESP_LOGI(TAG, "Already provisioned, starting Wi-Fi STA");
+
+        /* We don't need the manager as device is already provisioned,
+         * so let's release it's resources */
+        wifi_prov_mgr_deinit();
+
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+        ESP_ERROR_CHECK(esp_wifi_start());
+    }
+
+    /* Wait for Wi-Fi connection */
+    xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_EVENT, false, true, portMAX_DELAY);
+    return;
+}
