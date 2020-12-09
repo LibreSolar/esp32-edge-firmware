70f8a71e0d21c847e16f7492625ef112506c3cdf
Kathrin Weihe <kathrin.weihe@posteo.de>
2020-12-09 18:14:37 +0100

    Overhauled frontend, showing serial live data.
    Cleaned up whitespaces and linting errors.

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios'
 import store from './store'
+import axios from 'axios'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
-
 Vue.prototype.$ajax = axios
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
